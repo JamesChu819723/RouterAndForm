e874559962d4c159f0ff64cc8ca63a6ff74254a9
jameschu <james819723@gmail.com>
2019-09-22 14:35:11 +0800

    add route settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleComponent } from './article/article.component';
+import { ArticleCreateComponent } from './article-create/article-create.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'article', component: ArticleListComponent },
+  { path: 'article/create', component: ArticleCreateComponent },
+  { path: 'article/:id', component: ArticleComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [],
