e816d4edea02fbf38212e4367bbc3a81564c061e
jameschu <james819723@gmail.com>
2019-09-22 14:39:55 +0800

    create stand alone route setting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Route } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleComponent } from './article/article.component';
 import { ArticleCreateComponent } from './article-create/article-create.component';
 import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [
-  { path: '', children: [
+const childRouteSettings: Route = {
+  path: '',
+  children: [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'article', component: ArticleListComponent },
     { path: 'article/create', component: ArticleCreateComponent },
     { path: 'article/:id', component: ArticleComponent }
-  ]},
+  ]
+};
+
+const routes: Routes = [
+  childRouteSettings,
   { path: 'login', component: LoginComponent }
 ];
 
