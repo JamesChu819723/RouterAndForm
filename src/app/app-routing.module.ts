745b35f3c30d599ddf6c0aeaa37ecf14c04d4cb0
jameschu <james819723@gmail.com>
2019-09-22 23:41:58 +0800

    use auth guard to protect url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LayoutComponent } from './layout/layout.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: '',
     component: LayoutComponent,
+    canActivate: [AuthGuard],
     loadChildren: () => import('./features/features.module').then(m => m.FeaturesModule) // after angular 8
-    // loadChildren: ./features/features.module#FeaturesModule'
+    // loadChildren: './features/features.module#FeaturesModule'
   },
   { path: '**', redirectTo: '/home' }
 ];
