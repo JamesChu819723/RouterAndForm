4ff5c6aec0171045146616137f94a50599028db6
jameschu <james819723@gmail.com>
2019-09-22 14:27:11 +0800

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleComponent } from './article/article.component';
+import { ArticleCreateComponent } from './article-create/article-create.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ArticleListComponent,
+    ArticleComponent,
+    LoginComponent,
+    ArticleCreateComponent
   ],
   imports: [
     BrowserModule,
