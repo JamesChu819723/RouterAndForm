37f2e10bb605f6973e23b54685d6c06a33404538
jameschu <james819723@gmail.com>
2019-10-05 23:46:05 +0800

    create form group in article-create component

--- src/app/article-create/article-create.component.ts
+++ src/app/article-create/article-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-article-create',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-create.component.css']
 })
 export class ArticleCreateComponent implements OnInit {
-
+  form = new FormGroup(
+    {
+      author: new FormControl('', Validators.required ),
+      title: new FormControl('', Validators.required),
+      paragraphs: new FormArray([
+        new FormControl('', Validators.required),
+        new FormControl('', Validators.required)
+      ])
+    }
+  );
   constructor() { }
 
   ngOnInit() {
