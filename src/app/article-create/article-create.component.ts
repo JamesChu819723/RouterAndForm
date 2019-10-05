4bdf7104700c9e65c202dfd650518e864cd5acb1
jameschu <james819723@gmail.com>
2019-10-06 00:13:04 +0800

    use form builder to create form group

--- src/app/article-create/article-create.component.ts
+++ src/app/article-create/article-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormArray, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-article-create',
@@ -7,17 +7,25 @@ import { FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./article-create.component.css']
 })
 export class ArticleCreateComponent implements OnInit {
-  form = new FormGroup(
-    {
-      author: new FormControl('', Validators.required ),
-      title: new FormControl('', Validators.required),
-      paragraphs: new FormArray([
-        new FormControl('', Validators.required),
-        new FormControl('', Validators.required)
-      ])
-    }
-  );
-  constructor() { }
+  // form = new FormGroup(
+  //   {
+  //     author: new FormControl('james', Validators.required ),
+  //     title: new FormControl('A good news', [Validators.required, Validators.minLength(3)]),
+  //     paragraphs: new FormArray([
+  //       new FormControl('good paragraph 1', Validators.required),
+  //       new FormControl('good paragraph 2', Validators.required)
+  //     ])
+  //   }
+  // );
+  form = this.fb.group({
+    author: ['james', Validators.required],
+    title: ['A good news', [Validators.minLength(3)]],
+    paragraphs: this.fb.array([
+      ['good paragraph 1', Validators.required],
+      ['good paragraph 2', Validators.required]
+    ])
+  });
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
